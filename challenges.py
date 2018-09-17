0fe2359462de609bb74435c2016c3b8f683933ae
csabi <nemeth.csaba@revolve.hu>
2018-09-17 10:05:58 +0200

    coderbyte

---
+++ challenges.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#  2
+
+
+
+
+
+exit()
+
+# 1
+def FirstReverse(valami):
+    # code goes here
+    string = valami[::-1]
+    return str(string)
+
+# keep this function call here
+print FirstReverse(raw_input())
+
+exit()
+
