4a58017cbfc630eb006f0b0df0a51f2e183a4864
csabi <nemeth.csaba@revolve.hu>
2018-09-17 12:00:22 +0200

    coderbyte

--- challenges.py
+++ challenges.py
@@ -1,11 +1,52 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+# 3
+
+def SimpleAdding(num):
+    newNum = 0
+
+    # code goes here
+    for i in range(int(num) + 1):
+        newNum = newNum + i
+
+    return newNum
+
+# keep this function call here
+print SimpleAdding(raw_input())
+
+
+exit()
+
+
+
 #  2
 
-print "2222"
+def LetterChanges(valami):
+    newValami=""
+    newChar=""
 
+    # code goes here
+    # print ord("v")
 
+    for char in (valami):
+        if char.isalpha():
+            if char.lower() == "z":
+                newChar = 'a'
+            else:
+                newChar = chr(ord(char) + 1)
+        else:
+            newChar = char
+
+        if newChar in "aeiou":
+            newChar = newChar.upper()
+
+        newValami = newValami + newChar
+
+    return newValami
+
+# keep this function call here
+print LetterChanges(raw_input())
 
 exit()
 
